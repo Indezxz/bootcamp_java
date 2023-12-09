cc378c6dbd790201094c1357672a86de043f1fd0
Juan Suarez <116114043+Indezxz@users.noreply.github.com>
2023-12-09 16:13:46 -0500

    Update Main.java
    Se editó la clase Main

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Creacion de Una Clase X");
+        System.out.println("Creacion de Una Clase XYZ");
     }
 }
